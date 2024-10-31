4c0af8ec3286ea9b4cd677d624450f8f95d2e84f
Max Franck <max.franck@proton.me>
2024-10-31 15:09:35 -0300

    different select for different tables

--- ETL Pipeline/etl_pipeline_python.py
+++ ETL Pipeline/etl_pipeline_python.py
@@ -6,36 +6,61 @@ import os
 
 load_dotenv()
 
-server = os.getenv('DB_HOST')
-database = os.getenv('DB_NAME')
-uid = os.getenv('DB_USER')
-pwd = os.getenv('DB_PASSWORD')
+a_server = os.getenv('A_DB_HOST')
+a_database = os.getenv('A_DB_NAME')
+a_uid = os.getenv('A_DB_USER')
+a_pwd = os.getenv('A_DB_PASSWORD')
+b_server = os.getenv('B_DB_HOST')
+b_database = os.getenv('B_DB_NAME')
+b_uid = os.getenv('B_DB_USER')
+b_pwd = os.getenv('B_DB_PASSWORD')
 driver = "{ODBC Driver 18 for SQL Server}"
 
+def extract():
+    try:
+        connection_string = (
+            f'DRIVER={driver};SERVER={a_server};DATABASE={a_database};'
+            f'UID={a_uid};PWD={a_pwd};TrustServerCertificate=Yes'
+        )
+        connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string})
+        src_engine = create_engine(connection_url)
+        src_conn = src_engine.connect()
+        print("Conexão bem-sucedida!")
+
+        query = """
+            select  t.name as table_name
+            from sys.tables t
+            where t.name in ('TB_LOTE_LOCALIZACAO','TB_LOC_QUADRA')
+        """
+        src_tables = pd.read_sql_query(query, src_conn).to_dict()['table_name']
+
+        for id in src_tables:
+            table_name = src_tables[id]
+            if table_name == 'TB_LOTE_LOCALIZACAO':
+                df = pd.read_sql_query(f'select top 5 ID, IDLote, Pilha, posicao, IDEmpresa from {table_name}', src_conn)
+            else:
+                df = pd.read_sql_query(f'select top 5 ID, Nome, IDCorredor, posicao, pilha from {table_name}', src_conn)
+            load(df, table_name)
+
+        src_conn.close()
+
+    except Exception as e:
+        print("Data extract error: " + str(e))
+
+def load(df, tbl):
+    try:
+        rows_imported = 0
+        engine = create_engine(f'postgresql://{b_uid}:{b_pwd}@{b_server}:5432/{b_database}')
+        print(f'importing rows {rows_imported} to {rows_imported + len(df)}... for table {tbl}')
+
+        df.to_sql(f'stg_{tbl}', engine, if_exists='replace', index=False, chunksize=100000)
+        rows_imported += len(df)
+
+        print("Data imported successful")
+    except Exception as e:
+        print("Data load error: " + str(e))
+
 try:
-    connection_string = (
-        f'DRIVER={driver};SERVER={server};DATABASE={database};'
-        f'UID={uid};PWD={pwd};TrustServerCertificate=Yes'
-    )
-    connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string})
-    src_engine = create_engine(connection_url)
-    src_conn = src_engine.connect()
-    print("Conexão bem-sucedida!")
-
-    query = """
-        select  t.name as table_name
-        from sys.tables t
-        where t.name in ('TB_LOTE_LOCALIZACAO','TB_LOC_QUADRA')
-    """
-    src_tables = pd.read_sql_query(query, src_conn).to_dict()['table_name']
-
-    for id in src_tables:
-        table_name = src_tables[id]
-        df = pd.read_sql_query(f'select top 5 * FROM {table_name}', src_conn)
-        print(df)
-        # load(df, table_name)
-
-    src_conn.close()
-
+    extract()
 except Exception as e:
-    print("Data extract error: " + str(e))
+    print("Error while extracting data: " + str(e))
